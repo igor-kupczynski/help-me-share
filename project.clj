70637eb597e5b0df67b04f946ab4a0bb22bed9c9
Igor Kupczynski <puszczyk@gmail.com>
2013-06-18 23:24:49 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject help-me-share "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject help-me-share "0.1.0"
+  :description "A small javascript utility which adds 'Share' buttons for various services, i.e. twitter and facebook."
+  :url "https://github.com/puszczyk/help-me-share"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [domina "1.0.2-SNAPSHOT"]
+                 [domina "1.0.1"]
                  [hiccups "0.2.0"]]
 
 
